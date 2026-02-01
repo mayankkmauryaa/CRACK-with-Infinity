3497acbd10ed25b7491442a5e137fc8834e5c68f
Mayank <hpmayankmaurya@gmail.com>
2026-02-01 17:59:23 +0530

    part 1 - q3

--- HackWithInfy/Practice Questions 2025_Part 1/Easy/3/Solution.java
+++ HackWithInfy/Practice Questions 2025_Part 1/Easy/3/Solution.java
@@ -1,5 +1,32 @@
-public class Solution{
+
+import java.util.Scanner;
+
+public class Solution {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int c = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int ans = MinOilNeeded(arr, c);
+        System.out.println(ans);
+    }
+
+    private static int MinOilNeeded(int[] arr, int c) {
+        int disturbance = 0;
+        int oil = 0;
+        for (int i = 0; i < arr.length; i++) {
+            oil += arr[i];
+            if (oil < 0) {
+                disturbance += (-oil);
+                oil = 0;
+            } else if (oil > c) {
+                oil = c;
+            }
+        }
+        return disturbance;
     }
 }
