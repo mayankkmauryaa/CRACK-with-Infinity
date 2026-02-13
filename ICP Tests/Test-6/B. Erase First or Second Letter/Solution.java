b9dc5f5827762a9b4458f42d007de2e6a439805d
Mayank <hpmayankmaurya@gmail.com>
2026-02-13 23:26:48 +0530

    Test - 6

--- ICP Tests/Test-6/B. Erase First or Second Letter/Solution.java
+++ ICP Tests/Test-6/B. Erase First or Second Letter/Solution.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Solution {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
