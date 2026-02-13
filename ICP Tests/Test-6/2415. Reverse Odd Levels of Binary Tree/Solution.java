29ed7350c548aef1754350ad09c0645e02855fbf
Mayank <hpmayankmaurya@gmail.com>
2026-02-13 23:28:48 +0530

    Reverse Odd Levels of Binary Tree

--- ICP Tests/Test-6/2415. Reverse Odd Levels of Binary Tree/Solution.java
+++ ICP Tests/Test-6/2415. Reverse Odd Levels of Binary Tree/Solution.java
@@ -1,43 +1,108 @@
+import java.util.*;
 
 // Definition for a binary tree node.
 class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
+    TreeNode left, right;
 
     TreeNode(int val) {
         this.val = val;
     }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
 }
 
 public class Solution {
-    public TreeNode reverseOddLevels(TreeNode root) {
-        if (root != null) {
-            traverse(root.left, root.right, 0);
+
+    // Build tree from array (level order)
+    public static TreeNode buildTree(Integer[] arr) {
+        if (arr.length == 0 || arr[0] == null)
+            return null;
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        int i = 1;
+
+        while (!queue.isEmpty() && i < arr.length) {
+            TreeNode current = queue.poll();
+
+            if (i < arr.length && arr[i] != null) {
+                current.left = new TreeNode(arr[i]);
+                queue.add(current.left);
+            }
+            i++;
+
+            if (i < arr.length && arr[i] != null) {
+                current.right = new TreeNode(arr[i]);
+                queue.add(current.right);
+            }
+            i++;
         }
+
         return root;
     }
 
-    public static void traverse(TreeNode left, TreeNode right, int lvl) {
+    // Reverse odd levels
+    public static void reverseOddLevels(TreeNode root) {
+        if (root == null)
+            return;
+        dfs(root.left, root.right, 1);
+    }
+
+    private static void dfs(TreeNode left, TreeNode right, int level) {
         if (left == null || right == null)
             return;
 
-        if (lvl % 2 == 0) {
+        if (level % 2 == 1) {
             int temp = left.val;
             left.val = right.val;
             right.val = temp;
         }
 
-        traverse(left.left, right.right, lvl + 1);
-        traverse(left.right, right.left, lvl + 1);
+        dfs(left.left, right.right, level + 1);
+        dfs(left.right, right.left, level + 1);
+    }
+
+    // Print tree level order
+    public static void printLevelOrder(TreeNode root) {
+        if (root == null)
+            return;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.val + " ");
+
+            if (node.left != null)
+                queue.add(node.left);
+            if (node.right != null)
+                queue.add(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input:
+        // n
+        // array elements (use -1 for null if needed)
+        int n = sc.nextInt();
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++) {
+            int val = sc.nextInt();
+            if (val == -1)
+                arr[i] = null;
+            else
+                arr[i] = val;
+        }
+
+        TreeNode root = buildTree(arr);
+
+        reverseOddLevels(root);
+
+        printLevelOrder(root);
     }
 }
