72ad28e30bfdeebdd8ad90aa213bf1e0db9cd5f4
Mayank <hpmayankmaurya@gmail.com>
2026-02-20 13:01:46 +0530

    Sum of Query II - Prefix Sum Approach

--- Classroom-ICP-Phase-4/Week 5 - Tree/GFG-Sum of Query II/Solution.java
+++ Classroom-ICP-Phase-4/Week 5 - Tree/GFG-Sum of Query II/Solution.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Solution {
+class Solution_Brute {
     List<Integer> querySum(int n, int arr[], int q, int queries[]) {
 
         List<Integer> list = new ArrayList<>();
@@ -20,6 +20,32 @@ class Solution {
             list.add(sum);
         }
 
+        return list;
+    }
+}
+
+public class Solution {
+    List<Integer> querySum(int n, int arr[], int q, int queries[]) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        int[] prefix = new int[n + 1];
+        prefix[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + arr[i - 1];
+        }
+        
+        int idx = 0;
+        
+        for (int i = 0; i < q; i++) {
+            int l = queries[idx++];
+            int r = queries[idx++];
+            
+            int sum = prefix[r] - prefix[l - 1];
+            list.add(sum);
+        }
+        
         return list;
     }
 }
